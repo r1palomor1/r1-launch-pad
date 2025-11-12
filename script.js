be100a5039593adf9f525a71b45a29a63b0ebbeb
palomo32 <palomo32@gmail.com>
2025-11-11 19:48:42 -0600

    Working app: Polish Favorite icon in video cards and navigation/control rows

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 ﻿﻿/*
- Working app: Polish navigation bar collapse/uncollapse with scroll.
+ Working app: Polish Favorite icon in video cards and navigation/control rows.
+    Polish navigation bar collapse/uncollapse with scroll.
     Redesign YT player page to intergrate YT Search with 4 icons at top.
     Favorites icon for Songs and Playlists.  Saved to Favorites radio button.
     Auto hide controls in modes and PL overlay.  Use side icon to show.
